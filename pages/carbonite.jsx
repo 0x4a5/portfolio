c7ce44d5a96105a3906d8da91e90f1246b2d3043
0x4a5 <github@crisiscoins.com>
2022-06-10 13:17:27 -0700

    modified project links

--- pages/carbonite.jsx
+++ pages/carbonite.jsx
@@ -36,12 +36,12 @@ const carbonite = () => {
             sign in to their Google accounts through the implementation of oAuth
             2.0 open standard authentication protocol.
           </p>
-          <Link href="https://0x4a5.github.io/carbonite/">
+          <Link href="https://carbonite-sigma.vercel.app/">
             <a>
               <button className="px-8 py-2 mt-4 mr-8">Demo</button>
             </a>
           </Link>
-          <Link href="https://0x4a5.github.io/carbonite/">
+          <Link href="https://github.com/0x4a5/carbonite">
             <a>
               <button className="px-8 py-2 mt-4">Code</button>
             </a>
